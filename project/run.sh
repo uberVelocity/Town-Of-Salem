3f9e7c49fd1c5adb4572f383b2894df0a18cfa27
ubervelocity <mihaitu.popescu@gmail.com>
2020-06-05 18:55:01 +0200

    PATH determined dynamically

--- project/run.sh
+++ project/run.sh
@@ -1,17 +1,21 @@
+SCRIPT="/main.py"
+FILE=$(pwd)$SCRIPT
+RESULTS_NAME="/results"
+RESULTS=$(pwd)$RESULTS_NAME
 MAFIA="MAFIA WINS!"
 MAFIA_WINS=0
 TOWN_WINS=0
 for (( i=1; i <=$1; i++ ))
 do
-    RESULT=$(python3 /home/merkel/Documents/University/lamas/Town-Of-Salem/project/main.py)
+    RESULT=$(python3 $FILE)
     if [ "$RESULT"="$MAFIA" ]; then
         ((MAFIA_WINS=MAFIA_WINS+1))
-        printf "Mafia wins: %d" $MAFIA_WINS > /home/merkel/Documents/University/lamas/Town-Of-Salem/project/results
-        printf "\nTown wins: %d\n" $TOWN_WINS >> /home/merkel/Documents/University/lamas/Town-Of-Salem/project/results
+        printf "Mafia wins: %d" $MAFIA_WINS > $RESULTS
+        printf "\nTown wins: %d\n" $TOWN_WINS >> $RESULTS
     else
         ((TOWN_WINS=TOWN_WINS+1))
-        printf "Mafia wins: %d" $MAFIA_WINS > /home/merkel/Documents/University/lamas/Town-Of-Salem/project/results
-        printf "\nTown wins: %d\n" $TOWN_WINS >> /home/merkel/Documents/University/lamas/Town-Of-Salem/project/results
+        printf "Mafia wins: %d" $MAFIA_WINS > $RESULTS
+        printf "\nTown wins: %d\n" $TOWN_WINS >> $RESULTS
     fi
     echo $i
 done
