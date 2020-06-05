c04f44ba8e2227c31da4d50e1acf46cf3f11d1f5
ubervelocity <mihaitu.popescu@gmail.com>
2020-06-05 18:49:35 +0200

    Adds interrupt results support

--- project/run.sh
+++ project/run.sh
@@ -6,12 +6,14 @@ do
     RESULT=$(python3 /home/merkel/Documents/University/lamas/Town-Of-Salem/project/main.py)
     if [ "$RESULT"="$MAFIA" ]; then
         ((MAFIA_WINS=MAFIA_WINS+1))
+        printf "Mafia wins: %d" $MAFIA_WINS > /home/merkel/Documents/University/lamas/Town-Of-Salem/project/results
+        printf "\nTown wins: %d\n" $TOWN_WINS >> /home/merkel/Documents/University/lamas/Town-Of-Salem/project/results
     else
         ((TOWN_WINS=TOWN_WINS+1))
+        printf "Mafia wins: %d" $MAFIA_WINS > /home/merkel/Documents/University/lamas/Town-Of-Salem/project/results
+        printf "\nTown wins: %d\n" $TOWN_WINS >> /home/merkel/Documents/University/lamas/Town-Of-Salem/project/results
     fi
     echo $i
 done
 
-printf "Mafia wins: %d" $MAFIA_WINS > /home/merkel/Documents/University/lamas/Town-Of-Salem/project/results
 
-printf "\nTown wins: %d\n" $TOWN_WINS >> /home/merkel/Documents/University/lamas/Town-Of-Salem/project/results
